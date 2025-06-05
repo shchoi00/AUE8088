2954ea1c60e7d22860828a945534251ac8cd078b
shchoi00 <shchoi00@hanyang.ac.kr>
2025-06-05 13:56:56 +0000

    feat: implement random_perspective

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -54,6 +54,8 @@ from utils.general import (
     xywh2xyxy,
     xywhn2xyxy,
     xyxy2xywhn,
+    resample_segments,
+    segment2box,
 )
 
 from utils.metrics import bbox_ioa
@@ -160,6 +162,109 @@ def _copy_paste(im0, im1, labels, segments, p=0.5):
         im1[i] = result1[i]
 
     return im0, im1, labels, segments
+
+def box_candidates(box1, box2, wh_thr=2, ar_thr=100, area_thr=0.1, eps=1e-16):
+    """
+    Filters bounding box candidates by minimum width-height threshold `wh_thr` (pixels), aspect ratio threshold
+    `ar_thr`, and area ratio threshold `area_thr`.
+
+    box1(4,n) is before augmentation, box2(4,n) is after augmentation.
+    """
+    w1, h1 = box1[2] - box1[0], box1[3] - box1[1]
+    w2, h2 = box2[2] - box2[0], box2[3] - box2[1]
+    ar = np.maximum(w2 / (h2 + eps), h2 / (w2 + eps))  # aspect ratio
+    return (w2 > wh_thr) & (h2 > wh_thr) & (w2 * h2 / (w1 * h1 + eps) > area_thr) & (ar < ar_thr)  # candidates
+
+def _random_perspective(
+    im0, im1, targets=(), segments=(), degrees=10, translate=0.1, scale=0.1, shear=10, perspective=0.0, border=(0, 0)
+):
+    # torchvision.transforms.RandomAffine(degrees=(-10, 10), translate=(0.1, 0.1), scale=(0.9, 1.1), shear=(-10, 10))
+    # targets = [cls, xyxy]
+
+    height = im0.shape[0] + border[0] * 2  # shape(h,w,c)
+    width = im0.shape[1] + border[1] * 2
+
+    # Center
+    C = np.eye(3)
+    C[0, 2] = -im0.shape[1] / 2  # x translation (pixels)
+    C[1, 2] = -im0.shape[0] / 2  # y translation (pixels)
+
+    # Perspective
+    P = np.eye(3)
+    P[2, 0] = random.uniform(-perspective, perspective)  # x perspective (about y)
+    P[2, 1] = random.uniform(-perspective, perspective)  # y perspective (about x)
+
+    # Rotation and Scale
+    R = np.eye(3)
+    a = random.uniform(-degrees, degrees)
+    # a += random.choice([-180, -90, 0, 90])  # add 90deg rotations to small rotations
+    s = random.uniform(1 - scale, 1 + scale)
+    # s = 2 ** random.uniform(-scale, scale)
+    R[:2] = cv2.getRotationMatrix2D(angle=a, center=(0, 0), scale=s)
+
+    # Shear
+    S = np.eye(3)
+    S[0, 1] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # x shear (deg)
+    S[1, 0] = math.tan(random.uniform(-shear, shear) * math.pi / 180)  # y shear (deg)
+
+    # Translation
+    T = np.eye(3)
+    T[0, 2] = random.uniform(0.5 - translate, 0.5 + translate) * width  # x translation (pixels)
+    T[1, 2] = random.uniform(0.5 - translate, 0.5 + translate) * height  # y translation (pixels)
+
+    # Combined rotation matrix
+    M = T @ S @ R @ P @ C  # order of operations (right to left) is IMPORTANT
+    if (border[0] != 0) or (border[1] != 0) or (M != np.eye(3)).any():  # image changed
+        if perspective:
+            im0 = cv2.warpPerspective(im0, M, dsize=(width, height), borderValue=(114, 114, 114))
+            im1 = cv2.warpPerspective(im1, M, dsize=(width, height), borderValue=(114, 114, 114))
+        else:  # affine
+            im0 = cv2.warpAffine(im0, M[:2], dsize=(width, height), borderValue=(114, 114, 114))
+            im1 = cv2.warpAffine(im1, M[:2], dsize=(width, height), borderValue=(114, 114, 114))
+
+    # Visualize
+    # import matplotlib.pyplot as plt
+    # ax = plt.subplots(1, 2, figsize=(12, 6))[1].ravel()
+    # ax[0].imshow(im[:, :, ::-1])  # base
+    # ax[1].imshow(im2[:, :, ::-1])  # warped
+
+    # Transform label coordinates
+    n = len(targets)
+    if n:
+        use_segments = any(x.any() for x in segments) and len(segments) == n
+        new = np.zeros((n, 4))
+        if use_segments:  # warp segments
+            segments = resample_segments(segments)  # upsample
+            for i, segment in enumerate(segments):
+                xy = np.ones((len(segment), 3))
+                xy[:, :2] = segment
+                xy = xy @ M.T  # transform
+                xy = xy[:, :2] / xy[:, 2:3] if perspective else xy[:, :2]  # perspective rescale or affine
+
+                # clip
+                new[i] = segment2box(xy, width, height)
+
+        else:  # warp boxes
+            xy = np.ones((n * 4, 3))
+            xy[:, :2] = targets[:, [1, 2, 3, 4, 1, 4, 3, 2]].reshape(n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
+            xy = xy @ M.T  # transform
+            xy = (xy[:, :2] / xy[:, 2:3] if perspective else xy[:, :2]).reshape(n, 8)  # perspective rescale or affine
+
+            # create new boxes
+            x = xy[:, [0, 2, 4, 6]]
+            y = xy[:, [1, 3, 5, 7]]
+            new = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
+
+            # clip
+            new[:, [0, 2]] = new[:, [0, 2]].clip(0, width)
+            new[:, [1, 3]] = new[:, [1, 3]].clip(0, height)
+
+        # filter candidates
+        i = box_candidates(box1=targets[:, 1:5].T * s, box2=new.T, area_thr=0.01 if use_segments else 0.10)
+        targets = targets[i]
+        targets[:, 1:5] = new[i]
+
+    return im0, im1, targets
 
 # Inherit from DistributedSampler and override iterator
 # https://github.com/pytorch/pytorch/blob/master/torch/utils/data/distributed.py
@@ -1242,8 +1347,6 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
                 if random.random() < hyp["mixup"]:
                     imgs, labels = mixup(imgs, labels, *self.load_mosaic(random.choice(self.indices)))
 
-            # import pdb; pdb.set_trace()
-            print( imgs[0].shape[:1])
             h, w = imgs[0].shape[:2]
             for i in (0, 1):
                 imgs[i] = imgs[i].transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
@@ -1251,9 +1354,6 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
 
                 imgs[i] = torch.from_numpy(imgs[i])
 
-            # if labels.size:  # normalized xywh to pixel xyxy format
-            #         labels[:, 1:3] += labels[:, 3:5] / 2.0      # (x_lefttop, y_lefttop) -> (x_center, y_center)
-            #         labels[:, 1:] = xywhn2xyxy(labels[:, 1:], w, h, padw=pad[0], padh=pad[1])
 
             nl = len(labels)  # update after albumentations
             labels_out = torch.zeros((nl, 7))
@@ -1264,14 +1364,11 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
             # Load image
             # hw0s: original shapes, hw1s: resized shapes
             imgs, hw0s, hw1s = self.load_image(index)
-            print(hw1s)
-            print('img_size', self.img_size)
             for ii, (img, (h0, w0), (h, w)) in enumerate(zip(imgs, hw0s, hw1s)):
                 # Letterbox
                 shape = self.batch_shapes[self.batch[index]] if self.rect else self.img_size  # final letterboxed shape
                 img, ratio, pad = letterbox(img, shape, auto=False, scaleup=self.augment)
                 shapes = (h0, w0), (ratio, pad)  # for COCO mAP rescaling
-                print('shapes', shapes)
                 labels = self.labels[index].copy()
                 if labels.size:  # normalized xywh to pixel xyxy format
                     labels[:, 1:3] += labels[:, 3:5] / 2.0      # (x_lefttop, y_lefttop) -> (x_center, y_center)
@@ -1330,7 +1427,7 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
 
         # Drop occlusion level
         labels_out = labels_out[:, :-1]
-        print(labels_out)
+        print('lables_out', labels_out)
         return imgs, labels_out, self.im_files[index], shapes, index
 
     def load_image(self, i):
@@ -1375,7 +1472,6 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
         imgs4 = []
         labels4, segments4 = [], []
         s = self.img_size
-        print('img_size', self.img_size)
         yc, xc = (int(random.uniform(-x, 2 * s + x)) for x in self.mosaic_border)  # mosaic center x, y
         indices = [index] + random.choices(self.indices, k=3)  # 3 additional image indices
         random.shuffle(indices)
@@ -1385,7 +1481,6 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
 
             h = hw[0][0]
             w = hw[0][1]
-            print(h ,w)
 
             if i == 0:  # top left
                 img4_lwir= np.full((s * 2, s * 2, imgs[0].shape[2]), 114, dtype=np.uint8)  # base image with 4 tiles
@@ -1412,13 +1507,13 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
 
             # Labels
             labels, segments = self.labels[index].copy(), self.segments[index].copy()
-            print('labels', labels)
             if labels.size:
                 labels[:, 1:5] = xywhn2xyxy(
                     np.concatenate((labels[:, 1:3] + labels[:, 3:5] / 2,  labels[:, 3:5]), axis=1),
                     w, h, padw, padh
                 )
-                labels[:, 1:5] = xyxy2xywhn(labels[:, 1:5], 2*s, 2*s, clip=True, eps=1e-3)
+                # labels[:, 1:5] = xyxy2xywhn(labels[:, 1:5], 2*s, 2*s, clip=True, eps=1e-3)
+
 
                 segments = [xyn2xy(x, w, h, padw, padh) for x in segments]
             labels4.append(labels)
@@ -1430,30 +1525,24 @@ class LoadRGBTImagesAndLabels(LoadImagesAndLabels):
         for x in (labels4[:, 1:], *segments4):
             np.clip(x, 0, 2 * s, out=x)  # clip when using random_perspective()
         # img4, labels4 = replicate(img4, labels4)  # replicate
-        print(labels4)
+
         # Augment
         img4_lwir, img4_vis, labels4, segments4 = _copy_paste(img4_lwir, img4_vis, labels4, segments4, p=self.hyp["copy_paste"])
 
-        # # --- (8) (선택) random_perspective 증강 적용 ---
-        # # 기하학적 변형 행렬을 생성하여, 이미지와 라벨 좌표를 동시에 왜곡
-        # img4_lwir, labels4 = random_perspective(
-        #     img4_lwir, labels4, segments4,
-        #     degrees=self.hyp["degrees"],
-        #     translate=self.hyp["translate"],
-        #     scale=self.hyp["scale"],
-        #     shear=self.hyp["shear"],
-        #     perspective=self.hyp["perspective"],
-        #     border=self.mosaic_border,
-        # )
-        # img4_vis, labels4 = random_perspective(
-        #     img4_vis, labels4, segments4,
-        #     degrees=self.hyp["degrees"],
-        #     translate=self.hyp["translate"],
-        #     scale=self.hyp["scale"],
-        #     shear=self.hyp["shear"],
-        #     perspective=self.hyp["perspective"],
-        #     border=self.mosaic_border,
-        # )
+        img4_lwir, img4_vis , labels4 = _random_perspective(
+            img4_lwir,
+            img4_vis,
+            labels4,
+            segments4,
+            degrees=self.hyp["degrees"],
+            translate=self.hyp["translate"],
+            scale=self.hyp["scale"],
+            shear=self.hyp["shear"],
+            perspective=self.hyp["perspective"],
+            border=self.mosaic_border,
+        )  # border to remove
+
+        labels4[:, 1:5] = xyxy2xywhn(labels4[:, 1:5], s, s, clip=True, eps=1e-3)
 
         return [img4_lwir, img4_vis], labels4
 
